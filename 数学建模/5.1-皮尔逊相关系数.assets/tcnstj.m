141b3e8efd482b615a13f9d166086295d134def4
AiEson <32814260+AiEson@users.noreply.github.com>
2022-02-08 23:49:20 +0800

    update

---
+++ 数学建模/5.1-皮尔逊相关系数.assets/tcnstj.m
@@ -0,0 +1,10 @@
+load 'physical fitness test.mat'
+%% 统计描述
+MIN = min(Test);  % 每一列的最小值
+MAX = max(Test);   % 每一列的最大值
+MEAN = mean(Test);  % 每一列的均值
+MEDIAN = median(Test);  %每一列的中位数
+SKEWNESS = skewness(Test); %每一列的偏度
+KURTOSIS = kurtosis(Test);  %每一列的峰度
+STD = std(Test);  % 每一列的标准差
+RESULT = [MIN;MAX;MEAN;MEDIAN;SKEWNESS;KURTOSIS;STD]  %将这些统计量放到一个矩阵中表示
